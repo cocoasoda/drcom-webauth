01c2541b6079a77e2b1cc28129581218d342cf69
agentmario <2014190026@email.szu.edu.cn>
2016-09-02 13:49:32 +0800

    Typo fixes

--- run.py
+++ run.py
@@ -7,12 +7,12 @@ conf = {
     'url': 'http://172.30.255.2/a30.htm'
 }
 form = {
-    'DDDDD': conf.account,
-    'upass': conf.passwd,
+    'DDDDD': conf['account'],
+    'upass': conf['passwd'],
     '0MKKey': '%B5%C7%A1%A1%C2%BC'
 }
 #url, 0MKKey 请自行抓包 
 
 data = urllib.urlencode(form)
-req = urllib2.Request(conf.url, data)
+req = urllib2.Request(conf['url'], data)
 res = urllib2.urlopen(req)
