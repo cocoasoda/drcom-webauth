382b15ff50771b1a621c9e7317a66f907ce3f445
agentmario <2014190026@email.szu.edu.cn>
2016-09-02 13:37:57 +0800

    Initial version

---
+++ run.py
@@ -0,0 +1,18 @@
+import urllib
+import urllib2
+#深大某宿舍区样例
+conf = {
+    'account': 'YOUR_ACCOUNT',
+    'passwd': 'YOUR_PASSWORD',
+    'url': 'http://172.30.255.2/a30.htm'
+}
+form = {
+    'DDDDD': conf.account,
+    'upass': conf.passwd,
+    '0MKKey': '%B5%C7%A1%A1%C2%BC'
+}
+#url, 0MKKey 请自行抓包 
+
+data = urllib.urlencode(form)
+req = urllib2.Request(conf.url, data)
+res = urllib2.urlopen(req)
